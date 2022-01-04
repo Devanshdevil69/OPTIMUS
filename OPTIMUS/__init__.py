77d80a65772d820a0e7d0906fdff0564b4780b1a
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-04 20:35:23 +0530

    Update __init__.py

--- OPTIMUS/__init__.py
+++ OPTIMUS/__init__.py
@@ -9,7 +9,7 @@ from datetime import datetime
 API_ID = int(os.environ["API_ID"])
 API_HASH = os.environ["API_HASH"]
 SESSION = os.environ["SESSION_STRING"]
-BOT_TOKEN = os.environ["BOT_TOKEN"]
+
 HANDLER = os.environ["HANDLER"]
 
 
@@ -20,10 +20,6 @@ amaan = Client(
     api_id=API_ID,
     api_hash=API_HASH
 )
-
-# assisstant client
-
-amaan2 = Client("hehe", api_id=API_ID, api_hash=API_HASH, bot_token = BOT_TOKEN)
 
 # start time
 
