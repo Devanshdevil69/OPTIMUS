2f40acd00ac4e9417b18060101c302170090405d
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-04 20:37:24 +0530

    Update __main__.py

--- OPTIMUS/__main__.py
+++ OPTIMUS/__main__.py
@@ -1,12 +1,11 @@
 from OPTIMUS import *
-from OPTIMUS import amaan, amaan2
+from OPTIMUS import amaan
 
 from OPTIMUS.modules import *
-from OPTIMUS.modules.assistant import *
+
 
 
 
 
 if __name__ == "__main__":
     amaan.run()
-    amaan2.run()
