6ccb351b97f2a9a288aacf335e7e1b5e1a348e4d
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-04 19:58:28 +0530

    Update btc_miner.py

--- OPTIMUS/modules/assistant/btc_miner.py
+++ OPTIMUS/modules/assistant/btc_miner.py
@@ -5,7 +5,7 @@
 from hashlib import sha256
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from Vinci import god
+from OPTIMUS import amaan2
 from pyromod import listen
 
 
@@ -22,39 +22,39 @@ async def mine(block_number, transactions, previous_hash, prefix_zeros, MAX_NONC
       return new_hash
 
 
-@god.on_message(filters.command("btc") & filters.private)
+@amaan2.on_message(filters.command("btc") & filters.private)
 async def btc_mining(_, m: Message):
-  block_number = await god.ask(m.chat.id, "**Send me the** __Block Number__")
+  block_number = await amaan2.ask(m.chat.id, "**Send me the** __Block Number__")
   if block_number.text.startswith("/"):
     return
   else:
     pass
-  transactions = await god.ask(m.chat.id, "**Now send me your** __Wallet Address__")
+  transactions = await amaan2.ask(m.chat.id, "**Now send me your** __Wallet Address__")
   if transactions.text.startswith("/"):
     return
   else:
     pass
-  previous_hash = await god.ask(m.chat.id, "**Send me the** __Previous Hash__")
+  previous_hash = await amaan2.ask(m.chat.id, "**Send me the** __Previous Hash__")
   if previous_hash.text.startswith("/"):
     return
   else:
     pass
-  prefix_zeros = await god.ask(m.chat.id, "**Send me the value of** __Prefix Zeros__")
+  prefix_zeros = await amaan2.ask(m.chat.id, "**Send me the value of** __Prefix Zeros__")
   if prefix_zeros.text.startswith("/"):
     return
   else:
     pass
-  MAX_NONCE = await god.ask(m.chat.id, "**Ok now send the** __Nonce Value__")
+  MAX_NONCE = await amaan2.ask(m.chat.id, "**Ok now send the** __Nonce Value__")
   if MAX_NONCE.text.startswith("/"):
     return
   else:
     pass
-  await god.send_message(m.chat.id, "__Started Mining__ \n**Please do not send any command**\n\n**Note:- If you send any command during Mining then your mining will stop and you have to do all the steps again**")
+  await amaan2.send_message(m.chat.id, "__Started Mining__ \n**Please do not send any command**\n\n**Note:- If you send any command during Mining then your mining will stop and you have to do all the steps again**")
   hola = await mine(block_number, transactions, previous_hash, prefix_zeros, MAX_NONCE) 
   if hola:
-    await god.send_message(m.chat.id, f"**Yay! Succesfully mined bitcoins with nonce value:- {nonce}**")
-    await god.send_message(m.chat.id, new_hash)
+    await amaan2.send_message(m.chat.id, f"**Yay! Succesfully mined bitcoins with nonce value:- {nonce}**")
+    await amaan2.send_message(m.chat.id, new_hash)
   else:
-    await god.send_message(m.chat.id, "Coudn't find correct hash after trying {MAX_NONCE} times")
+    await amaan2.send_message(m.chat.id, "Coudn't find correct hash after trying {MAX_NONCE} times")
   
   
