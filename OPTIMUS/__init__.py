bb3e17abacb666c4923ef10eaace38918eee67f2
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-04 19:19:13 +0530

    Update __init__.py

--- OPTIMUS/__init__.py
+++ OPTIMUS/__init__.py
@@ -9,12 +9,13 @@ API_ID = int(os.environ["API_ID"])
 API_HASH = os.environ["API_HASH"]
 SESSION = os.environ["SESSION_STRING"]
 BOT_TOKEN = os.environ["BOT_TOKEN"]
+HANDLER = os.environ["HANDLER"]
 
-arpi = Client(
+amaan = Client(
     SESSION,
     api_id=API_ID,
     api_hash=API_HASH
 )
 
-god = Client("godboyx", api_id=API_ID, api_hash=API_HASH, bot_token = BOT_TOKEN)
+amaan2 = Client("hehe", api_id=API_ID, api_hash=API_HASH, bot_token = BOT_TOKEN)
 
