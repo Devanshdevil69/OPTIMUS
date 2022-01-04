68143ac7e17e94413673f27bcac5406c5c5b0c7d
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-04 19:50:17 +0530

    Update __init__.py

--- OPTIMUS/__init__.py
+++ OPTIMUS/__init__.py
@@ -3,6 +3,7 @@
 
 import os
 from pyrogram import Client
+from datetime import datetime
 #from plugins import *
 
 API_ID = int(os.environ["API_ID"])
@@ -11,11 +12,19 @@ SESSION = os.environ["SESSION_STRING"]
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 HANDLER = os.environ["HANDLER"]
 
+
+# client
+
 amaan = Client(
     SESSION,
     api_id=API_ID,
     api_hash=API_HASH
 )
 
+# assisstant client
+
 amaan2 = Client("hehe", api_id=API_ID, api_hash=API_HASH, bot_token = BOT_TOKEN)
 
+# start time
+
+StartTime = datetime.now()
