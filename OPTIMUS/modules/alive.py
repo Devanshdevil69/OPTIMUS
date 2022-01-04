a958fba66c01b4cf153d28050934288d4eea2ef2
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-04 19:46:56 +0530

    Update alive.py

--- OPTIMUS/modules/alive.py
+++ OPTIMUS/modules/alive.py
@@ -1,22 +1,27 @@
-from Vinci import arpi
+from OPTIMUS import amaan, HANDLER, StartTime
 from pyrogram import filters, __version__
 from sys import version_info
+from datetime import datetime
+import asyncio
 
 
-
-
-@arpi.on_message(filters.command("ping", ",") & filters.me)
+@amaan.on_message(filters.command("ping", HANDLER) & filters.me)
 async def ping(client, message):
-    await message.edit_text("🏓Pong!")
+    start = datetime.now()
+    await message.edit_text("`Pong!`")
+    end = datetime.now()
+    m_s = (end - start).microseconds / 1000 
+    await message.edit_text(f"**🏓Pong!**\n`{m_s} ms`")
 
 
 
-@arpi.on_message(filters.command("alive", ",") & filters.me)
+@amaan.on_message(filters.command("alive", HANDLER) & filters.me)
 async def alive(client, message):
-    text="**VINCI USERBOT**\n"
+    text="**OPTIMUS USERBOT**\n"
     text += f"\nPython Version: `{version_info[0]}.{version_info[1]}.{version_info[2]}`"
     text += f"\nPyrogram Version: {__version__}"
-
+    text += f"\nCurrent Uptime: `{str(datetime.now() - StartTime).split('.')[0]}`"
+    
     await message.edit_text(text)
 
     
