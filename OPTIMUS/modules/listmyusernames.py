877e0ed4a84be317098968085c054b8d4cf18966
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-07 10:31:37 +0530

    Create listmyusernames.py

---
+++ OPTIMUS/modules/listmyusernames.py
@@ -0,0 +1,14 @@
+from pyrogram import filters
+from pyrogram.types import ChatPermissions
+from OPTIMUS import amaan, HANDLER
+import asyncio
+
+
+
+@amaan.on_message(filters.command("listmyusernames", HANDLER) & filters.me)
+async def mine(client, message):
+    result = await client(GetAdminedPublicChannelsRequest())
+    output_str = ""
+    for channel_obj in result.chats:
+        output_str += f"MY USERNAMES :-\n{channel_obj.title}\n@{channel_obj.username}\n\n"
+    await message.edit_text(output_str)
