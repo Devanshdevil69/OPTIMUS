865416ecb0e17bcbc7638688405660f3cf1bdc7d
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-10 22:55:23 +0530

    Update selfdistructive.py

--- OPTIMUS/modules/selfdistructive.py
+++ OPTIMUS/modules/selfdistructive.py
@@ -5,7 +5,6 @@ from OPTIMUS import amaan, HANDLER
 async def seddis(client, message):
   if not message.reply_to_message:
     return await message.edit_text('Reply to a self distructing media !.!.!')
-  await message.delete()
-  download_location = await client.download_media(message=message.reply_to_message,file_name='root/OPTIMUS/')
-  pic = await message.download()
+    k = await.client.message.reply_to_message
+    pic = await message.download()
   await client.send_message("me", pic)
