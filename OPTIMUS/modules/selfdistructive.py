97cebccad7a134f14f71dce9f36e8a6305d54028
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-10 23:22:49 +0530

    Update selfdistructive.py

--- OPTIMUS/modules/selfdistructive.py
+++ OPTIMUS/modules/selfdistructive.py
@@ -3,9 +3,9 @@ from OPTIMUS import amaan, HANDLER
 
 @amaan.on_message(filters.command("selfd", HANDLER) & filters.me)
 async def selfd(client, message):
+  await message.delete()
   if not message.reply_to_message:
     return await message.edit_text('Reply to a self distructing media !.!.!')
   k = message.reply_to_message
   pic = await k.download()
-  await message.delete()
   await client.send_document("me", pic)
