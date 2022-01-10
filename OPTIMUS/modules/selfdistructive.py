cb2fc8175331a2b2f315da7acd55e3c6b8440ed6
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-10 23:03:34 +0530

    Update selfdistructive.py

--- OPTIMUS/modules/selfdistructive.py
+++ OPTIMUS/modules/selfdistructive.py
@@ -2,10 +2,10 @@ from pyrogram import filters
 from OPTIMUS import amaan, HANDLER
 
 @amaan.on_message(filters.command("selfd", HANDLER) & filters.me)
-async def seddis(client, message):
+async def selfd(client, message):
   if not message.reply_to_message:
     return await message.edit_text('Reply to a self distructing media !.!.!')
-    k = await.message.reply_to_message
-    pic = await k.download()
-    await message.delete()
-    await client.send_message("me", pic)
+  k = message.reply_to_message
+  pic = await k.download()
+  await message.delete()
+  await client.send_message("me", pic)
