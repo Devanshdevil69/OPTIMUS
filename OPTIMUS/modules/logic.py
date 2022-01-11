63f502268d9687d9a72bbfada98b7c5353a72a45
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-11 12:51:48 +0530

    Update logic.py

--- OPTIMUS/modules/logic.py
+++ OPTIMUS/modules/logic.py
@@ -107,90 +107,76 @@ async def logic(client, message):
         await message.edit_text("`\"The biggest irony is that computers & mobiles were invented to save out time!\"`")
 
     if x==31:
-        await event.edit("`\"After honey was first discovered, there was likely a period where people were taste testing any available slime from insects.\"`")
+        await message.edit_text("`\"After honey was first discovered, there was likely a period where people were taste testing any available slime from insects.\"`")
 
     if x==32:
-        await event.edit("`\"You know you?re getting old when your parents start disappointing you, instead of you disappointing them.\"`")
+        await message.edit_text("`\"You know you?re getting old when your parents start disappointing you, instead of you disappointing them.\"`")
 
     if x==33:
-        await event.edit("`\"Humans are designed to learn through experience yet the education system has made it so we get no experience.\"`")
+        await message.edit_text("`\"Humans are designed to learn through experience yet the education system has made it so we get no experience.\"`")
 
     if x==34:
-        await event.edit("`\"By focusing on blinking, you blink slower... Same for breathing.\"`")
+        await message.edit_text("`\"By focusing on blinking, you blink slower... Same for breathing.\"`")
 
     if x==35:
-        await event.edit("`\"Drivers in a hurry to beat traffic usually cause the accidents which create the traffic they were trying to avoid.\"`")
+        await message.edit_text("`\"Drivers in a hurry to beat traffic usually cause the accidents which create the traffic they were trying to avoid.\"`")
 
     if x==36:
-        await event.edit("`\"Characters that get married in fiction were literally made for each other.\"`")
+        await message.edit_text("`\"Characters that get married in fiction were literally made for each other.\"`")
 
     if x==37:
-        await event.edit("`\"Babies are a clean hard drive that can be programmed with any language.\"`")
+        await message.edit_text("`\"Babies are a clean hard drive that can be programmed with any language.\"`")
 
     if x==38:
-        await event.edit("`\"There could be a miracle drug that cures every disease to man, that we'll never know about because it doesn't work on rats.\"`")
+        await message.edit_text("`\"There could be a miracle drug that cures every disease to man, that we'll never know about because it doesn't work on rats.\"`")
 
     if x==39:
-        await event.edit("`\"Rhinos evolved to grow a horn for protection, but it's what's making them go extinct.\"`")
+        await message.edit_text("`\"Rhinos evolved to grow a horn for protection, but it's what's making them go extinct.\"`")
 
     if x==40:
-        await event.edit("`\"Maybe we don't find time travelers because we all die in 25-50 years.\"`")
+        await message.edit_text("`\"Maybe we don't find time travelers because we all die in 25-50 years.\"`")
 
     if x==41:
-
-        await event.edit("`\"Sleep is the trial version of death, It even comes with ads based on your activity.\"`")
+        await message.edit_text("`\"Sleep is the trial version of death, It even comes with ads based on your activity.\"`")
 
     if x==42:
-
-        await event.edit("`\"The most unrealistic thing about Spy movies is how clean the air ventilation system is!\"`")
+        await message.edit_text("`\"The most unrealistic thing about Spy movies is how clean the air ventilation system is!\"`")
 
     if x==43:
-
-        await event.edit("`\"In games we play through easy modes to unlock hard modes. In life we play through hard modes to unlock easy modes.\"`")
+        await message.edit_text("`\"In games we play through easy modes to unlock hard modes. In life we play through hard modes to unlock easy modes.\"`")
 
     if x==44:
-
-        await event.edit("`\"Silent people seem smarter than loud people, because they keep their stupid thoughts to themselves.\"`")
+        await message.edit_text("`\"Silent people seem smarter than loud people, because they keep their stupid thoughts to themselves.\"`")
 
     if x==45:
-
-        await event.edit("`\"If Greenland actually turns green, we're all screwed.\"`")
+        await message.edit_text("`\"If Greenland actually turns green, we're all screwed.\"`")
 
     if x==46:
-
-        await event.edit("`\"If someone says clever things in your dream, it actually shows your own cleverness.\"`")
+        await message.edit_text("`\"If someone says clever things in your dream, it actually shows your own cleverness.\"`")
 
     if x==47:
-
-        await event.edit("`\"Famous movie quotes are credited to the actor and not the actual writer who wrote them.\"`")
+        await message.edit_text("`\"Famous movie quotes are credited to the actor and not the actual writer who wrote them.\"`")
 
     if x==48:
-
-        await event.edit("`\"No one actually teaches you how to ride a bicycle. They just hype you up until you work it out.\"`")
+        await message.edit_text("`\"No one actually teaches you how to ride a bicycle. They just hype you up until you work it out.\"`")
 
     if x==49:
-
-        await event.edit("`\"Ask yourself why the the brain ignores the second the.\"`")
+        await message.edit_text("`\"Ask yourself why the the brain ignores the second the.\"`")
 
     if x==50:
-
-        await event.edit("`\"You?ve probably forgot about 80% of your entire life and most of the memories you do remember are not very accurate to what actually happened.\"`")
+        await message.edit_text("`\"You?ve probably forgot about 80% of your entire life and most of the memories you do remember are not very accurate to what actually happened.\"`")
 
     if x==51:
-
-        await event.edit("`\"It will be a lot harder for kids to win against their parents in video games in the future.\"`")
+        await message.edit_text("`\"It will be a lot harder for kids to win against their parents in video games in the future.\"`")
 
     if x==52:
-
-        await event.edit("`\"Everyone has flaws, if you don't recognize yours, you have a new one.\"`")
+        await message.edit_text("`\"Everyone has flaws, if you don't recognize yours, you have a new one.\"`")
 
     if x==53:
-
-        await event.edit("`\"Raising a child is training your replacement.\"`")
+        await message.edit_text("`\"Raising a child is training your replacement.\"`")
 
     if x==54:
-
-        await event.edit("`\"'O'pen starts with a Closed circle, and 'C'lose starts with an open circle.\"`")
+        await message.edit_text("`\"'O'pen starts with a Closed circle, and 'C'lose starts with an open circle.\"`")
 
     if x==55:
         await message.edit_text("`\"There's always someone who hated you for no reason, and still does.\"`")
@@ -199,85 +185,85 @@ async def logic(client, message):
         await message.edit_text("`\"After popcorn was discovered, there must have been a lot of random seeds that were roasted to see if it would have the same effect.\"`")
 
     if x==57:
-        await event.edit("`\"The more important a good night's sleep is, the harder it is to fall asleep.\"`")
+        await message.edit_text("`\"The more important a good night's sleep is, the harder it is to fall asleep.\"`")
 
     if x==58:
-        await event.edit("`\"Blessed are those that can properly describe the type of haircut they want to a new stylist.\"`")
+        await message.edit_text("`\"Blessed are those that can properly describe the type of haircut they want to a new stylist.\"`")
 
     if x==59:
-        await event.edit("`\"Too many people spend money they haven't earned, to buy things they don't want, to impress people they don't like!\"`")
+        await message.edit_text("`\"Too many people spend money they haven't earned, to buy things they don't want, to impress people they don't like!\"`")
 
     if x==60:
-        await event.edit("`\"Theme park employees must be good at telling the difference between screams of horror and excitement.\"`")
+        await message.edit_text("`\"Theme park employees must be good at telling the difference between screams of horror and excitement.\"`")
 
     if x==61:
-        await event.edit("`\"6 to 6:30 feels more half-an-hour than 5:50 to 6:20\"`")
+        await message.edit_text("`\"6 to 6:30 feels more half-an-hour than 5:50 to 6:20\"`")
 
     if x==62:
-        await event.edit("`\"Getting your password right on the last login attempt before lockout is the closest thing to disarming a bomb at the last minute that most of us will experience.\"`")
+        await message.edit_text("`\"Getting your password right on the last login attempt before lockout is the closest thing to disarming a bomb at the last minute that most of us will experience.\"`")
 
     if x==63:
-        await event.edit("`\"Listening to podcasts before bed is the adult version of story-time.\"`")
+        await message.edit_text("`\"Listening to podcasts before bed is the adult version of story-time.\"`")
 
     if x==64:
-        await event.edit("`\"If all criminals stopped robbing then the security industry would fall in which they could then easily go back to robbing.\"`")
+        await message.edit_text("`\"If all criminals stopped robbing then the security industry would fall in which they could then easily go back to robbing.\"`")
 
     if x==65:
-        await event.edit("`\"A ton of whales is really only like half a whale.\"`")
+        await message.edit_text("`\"A ton of whales is really only like half a whale.\"`")
 
     if x==66:
-        await event.edit("`\"When you get old, the old you is technically the new you, and your young self is the old you.\"`")
+        await message.edit_text("`\"When you get old, the old you is technically the new you, and your young self is the old you.\"`")
 
     if x==67:
-        await event.edit("`\"You probably won't find many negative reviews of parachutes on the Internet.\"`")
+        await message.edit_text("`\"You probably won't find many negative reviews of parachutes on the Internet.\"`")
 
     if x==68:
-        await event.edit("`\"We show the most love and admiration for people when they're no longer around to appreciate it.\"`")
+        await message.edit_text("`\"We show the most love and admiration for people when they're no longer around to appreciate it.\"`")
 
     if x==69:
-        await event.edit("`\"We've practiced sleeping thousands of times, yet can't do it very well or be consistent.\"`")
+        await message.edit_text("`\"We've practiced sleeping thousands of times, yet can't do it very well or be consistent.\"`")
 
     if x==70:
-        await event.edit("`\"Humans are more enthusiastic about moving to another planet with hostile environment than preserving earth - the planet they are perfectly shaped for.\"`")
+        await message.edit_text("`\"Humans are more enthusiastic about moving to another planet with hostile environment than preserving earth - the planet they are perfectly shaped for.\"`")
 
     if x==71:
-        await event.edit("`\"The happiest stage of most people's lives is when their brains aren't fully developed yet.\"`")
+        await message.edit_text("`\"The happiest stage of most people's lives is when their brains aren't fully developed yet.\"`")
 
     if x==72:
-        await event.edit("`\"The most effective alarm clock is a full bladder.\"`")
+        await message.edit_text("`\"The most effective alarm clock is a full bladder.\"`")
 
     if x==73:
-        await event.edit("`\"You probably just synchronized blinks with millions of people.\"`")
+        await message.edit_text("`\"You probably just synchronized blinks with millions of people.\"`")
 
     if x==74:
-        await event.edit("`\"Since we test drugs on animals first, rat medicine must be years ahead of human medicine.\"`")
+        await message.edit_text("`\"Since we test drugs on animals first, rat medicine must be years ahead of human medicine.\"`")
 
     if x==75:
-        await event.edit("`\"Night before a day off is more satisfying than the actual day off.\"`")
+        await message.edit_text("`\"Night before a day off is more satisfying than the actual day off.\"`")
 
     if x==76:
-        await event.edit("`\"We put paper in a folder to keep it from folding.\"`")
+        await message.edit_text("`\"We put paper in a folder to keep it from folding.\"`")
 
     if x==77:
-        await event.edit("`\"Somewhere, two best friends are meeting for the first time.\"`")
+        await message.edit_text("`\"Somewhere, two best friends are meeting for the first time.\"`")
 
     if x==78:
-        await event.edit("`\"Our brain simultaneously hates us, loves us, doesn't care about us, and micromanages our every move.\"`")
+        await message.edit_text("`\"Our brain simultaneously hates us, loves us, doesn't care about us, and micromanages our every move.\"`")
 
     if x==79:
-        await event.edit("`\"Being a male is a matter of birth. Being a man is a matter of age. But being a gentleman is a matter of choice.\"`")
+        await message.edit_text("`\"Being a male is a matter of birth. Being a man is a matter of age. But being a gentleman is a matter of choice.\"`")
 
     if x==80:
-        await event.edit("`\"Soon the parents will be hiding their social account from their kids rather than kids hiding their accounts from the parents.\"`")
+        await message.edit_text("`\"Soon the parents will be hiding their social account from their kids rather than kids hiding their accounts from the parents.\"`")
 
     if x==81:
-        await event.edit("`\"Wikipedia is what the internet was meant to be.\"`")
+        await message.edit_text("`\"Wikipedia is what the internet was meant to be.\"`")
 
     if x==82:
-        await event.edit("`\"A theme park is the only place that you can hear screams in the distance and not be concerned.\"`")
+        await message.edit_text("`\"A theme park is the only place that you can hear screams in the distance and not be concerned.\"`")
 
     if x==83:
-        await event.edit("`\"A wireless phone charger offers less freedom of movement than a wired one.\"`")
+        await message.edit_text("`\"A wireless phone charger offers less freedom of movement than a wired one.\"`")
 
     if x==84:
         await message.edit_text("`\"If you repeatedly criticize someone for liking something you don't, they won't stop liking it. They'll stop liking you.\"`")
