2e6c6e172590ba7580667fea3f3cb761a43b73c5
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-11 19:43:56 +0530

    Update fun.py

--- OPTIMUS/modules/fun.py
+++ OPTIMUS/modules/fun.py
@@ -2,11 +2,18 @@ import asyncio
 from OPTIMUS import amaan, HANDLER
 from pyrogram import filters
 
+__PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
+__HELP__ = f"""
+**FUN MODULES**
+• {HANDLER}kill
+• {HANDLER}mf
+"""
+
 # KiLL
 
 @amaan.on_message(filters.command("kill", HANDLER) & filters.me)
 async def kill(client, message):
-   await message.edit_text("User Killed Sucessfully  ▀̿ ̿Ĺ̯̿̿▀̿ ̿")
+   await message.edit_text("UseR KilleD Sucessfully  ▀̿ ̿Ĺ̯̿̿▀̿ ̿")
 
 # MF
 
