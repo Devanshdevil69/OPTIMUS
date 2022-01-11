79a09d5bb3585341d16197d431115a4e480bc13b
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-11 18:02:57 +0530

    Update __init__.py

--- OPTIMUS/modules/__init__.py
+++ OPTIMUS/modules/__init__.py
@@ -6,7 +6,17 @@ from OPTIMUS import amaan
 from os.path import dirname, basename, isfile, join
 import glob
 
-modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = [
-    basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
-]
+def __list_all_plugins():
+
+    # This generates a list of plugins in this folder for the * in __main__ to work.
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    all_plugins = [
+        basename(f)[:-3]
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+    return all_plugins
+
+
+ALL_PLUGINS = sorted(__list_all_plugins())
+__all__ = ALL_PLUGINS + ["ALL_PLUGINS"]
