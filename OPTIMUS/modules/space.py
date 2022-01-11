c69f2ca7b7678a009b966e881bb7ce70fb337514
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-11 17:35:04 +0530

    Update space.py

--- OPTIMUS/modules/space.py
+++ OPTIMUS/modules/space.py
@@ -1,6 +1,16 @@
 from pyrogram import filters
 from OPTIMUS import amaan, HANDLER
 
+__PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
+__HELP__ = f"""
+**JUST FOR FUN PURPOSE**
+USAGE :-
+{HANDLER}space
+{HANDLER}blank
+
+"""
+
+
 # S P A C E
 
 @amaan.on_message(filters.command("space", HANDLER) & filters.me)
