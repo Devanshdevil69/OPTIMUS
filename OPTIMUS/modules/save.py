1609c8d0d20f6cfe1631442c35adc9ad3ec34588
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-11 17:48:16 +0530

    Update save.py

--- OPTIMUS/modules/save.py
+++ OPTIMUS/modules/save.py
@@ -1,5 +1,12 @@
+import os
 from pyrogram import filters
 from OPTIMUS import amaan, HANDLER
+
+__PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
+__HELP__ = f"""
+**SEND TAGGED MESSAGE TO SAVED MESSAGES**
+USAGE :- {HANDLER}save
+""" 
 
 @amaan.on_message(filters.command("save", HANDLER) & filters.me)
 async def to_saved(client, message):
