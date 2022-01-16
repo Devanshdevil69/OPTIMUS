3ae075701e44b34e47e8137ada953cd721c06078
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-01-17 00:11:03 +0530

    Create pmto.py

---
+++ OPTIMUS/modules/pmto.py
@@ -0,0 +1,34 @@
+# ported from telebot
+# i ported from shivam's project :) 
+# rewritten in pyro by @CRiMiNaL786 
+
+import os
+from pyrogram import filters
+from OPTIMUS import amaan, HANDLER
+
+__PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
+__HELP__ = f"""
+**TO PM SOMEONE**
+• `{HANDLER}pmto` <username> <message>
+"""
+
+
+@amaan.on_message(filters.command("pmto", HANDLER) & filters.me)
+async def pmto(client, message):
+    a = message.pattern_match.group(1)
+    b = a.split(" ")
+    chat_id = b[0]
+    try:
+        chat_id = int(chat_id)
+    except BaseException:
+        pass
+    msg = ""
+    for i in b[1:]:
+        msg += i + " "
+    if msg == "":
+        return
+    try:
+        await client.send_message(chat_id, msg)
+        await message.edit_text("Message sent!")
+    except BaseException:
+        await message.edit_text("Something went wrong.")
