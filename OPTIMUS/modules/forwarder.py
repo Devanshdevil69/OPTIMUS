e3ce9c4448e72513bb90beaacc6350338dded6f1
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-04-09 12:25:58 +0530

    Update forwarder.py

--- OPTIMUS/modules/forwarder.py
+++ OPTIMUS/modules/forwarder.py
@@ -1,6 +1,6 @@
 # FULLY MaKeD AND iDea oF @CRiMiNaL786
 # To ReCovER His LosT AccoUNT of @GaNGsTeR_xD
-"""
+
 import os
 import asyncio
 from OPTIMUS import amaan, HANDLER
@@ -8,6 +8,7 @@ from pyrogram import filters
 
 @amaan.on_message(filters.command("forward", HANDLER) & filters.me)
 async def forwader(client, message):
-   a = message.forward(chat_id)
+   a = amaan.get_messages(777000, 12345)
+   message.forward(511112479, a)
 
-"""
+
