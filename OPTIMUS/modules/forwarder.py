c5d26626bf42bc83179dbec5494ebba2141768ca
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-04-14 19:26:08 +0530

    Update forwarder.py

--- OPTIMUS/modules/forwarder.py
+++ OPTIMUS/modules/forwarder.py
@@ -9,6 +9,6 @@ from pyrogram import filters
 @amaan.on_message(filters.command("forward", HANDLER) & filters.me)
 async def forwader(client, message):
    amaan.get_messages(777000)
-   await amaan.message.forward(511112479)
+   await amaan.send_message(511112479)
 
 
