4e4e34594fbf871459f6020539798c4d703481d2
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-04-18 20:54:38 +0530

    Create sudo.py

---
+++ OPTIMUS/helpers/sudo.py
@@ -0,0 +1,11 @@
+# Made by @CRiMiNaL786
+# sasta nasha krke bnaya
+
+import asyncio
+import pyrogram
+from pyrogram import filters
+
+
+async def sudo_handler
+if message.from_user.id == 511112479
+            return await func(client, message)
