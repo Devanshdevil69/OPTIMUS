01b2f854647cdfd3e54e53a493fc58852d0e4f42
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-04-22 12:23:58 +0530

    Update pmto.py

--- OPTIMUS/modules/pmto.py
+++ OPTIMUS/modules/pmto.py
@@ -6,6 +6,7 @@ import os
 from pyrogram import filters
 from pyrogram.types import Chat
 from OPTIMUS import amaan, HANDLER
+from OPTIMUS import SUDO_USERS, SUDO_HANDLER
 
 __PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
 __HELP__ = f"""
