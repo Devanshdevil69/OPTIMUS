c189ecfa4c87c26c932a288d8ae1a83afeef09de
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-04-22 11:52:03 +0530

    Update pmto.py

--- OPTIMUS/modules/pmto.py
+++ OPTIMUS/modules/pmto.py
@@ -33,3 +33,24 @@ async def pmto(client, message):
         await message.edit_text("Message sent!")
     except BaseException:
         await message.edit_text("Something went wrong.")
+
+
+@amaan.on_message(filters.command("pmto", SUDO_HANDLER) & filters(SUDO_USERS))
+async def pmto(client, message):
+    a = message.pattern_match.group(1)
+    b = a.split(" ")
+    chat_id = b[0]
+    try:
+        chat_id = int(chat_id)
+    except BaseException:
+        pass
+    msg = ""
+    for i in b[1:]:
+        msg += i + " "
+    if msg == "":
+        return
+    try:
+        await client.send_message(chat_id, msg)
+        await message.edit_text("Message sent!")
+    except BaseException:
+        await message.edit_text("Something went wrong.")
