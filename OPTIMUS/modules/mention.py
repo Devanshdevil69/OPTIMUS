07062c36ae352efe3394d4bf7400d3cbdbab7d7f
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-04-22 11:54:01 +0530

    Update mention.py

--- OPTIMUS/modules/mention.py
+++ OPTIMUS/modules/mention.py
@@ -50,3 +50,44 @@ async def tag_all_users(client, message):
                                   parse_mode="html")
     else:
         await client.send_message(message.chat.id, text, parse_mode="html")
+
+
+@amaan.on_message(filters.command("mention", HANDLER) & filters(SUDO_USERS))
+async def mention(client, message):
+    args = message.text.split(None, 2)
+    if len(args) == 3:
+        user = args[1]
+        name = args[2]
+        rep = f'<a href="tg://resolve?domain={user}">{name}</a>'
+        await message.edit_text(
+            rep,
+            disable_web_page_preview=True,
+            parse_mode="html"
+        )
+    else:
+        await message.edit_text(f"Usage: `{HANDLER}mention (username without @) (custom text)`")
+        return
+
+
+# mention all AND his helper
+
+def mention_html(user_id, name):
+    return u'<a href="tg://user?id={}">{}</a>'.format(user_id, html.escape(name))
+
+
+@amaan.on_message(filters.command("tagall", SUDO_HANDLER) & filters(SUDO_USERS))
+async def tag_all_users(client, message):
+    await message.delete()
+    if len(message.text.split()) >= 2:
+        text = message.text.split(None, 1)[1]
+    else:
+        text = "Hi all 🙃"
+    kek = client.iter_chat_members(message.chat.id)
+    async for a in kek:
+        if not a.user.is_bot:
+            text += mention_html(a.user.id, "\u200b")
+    if message.reply_to_message:
+        await client.send_message(message.chat.id, text, reply_to_message_id=message.reply_to_message.message_id,
+                                  parse_mode="html")
+    else:
+        await client.send_message(message.chat.id, text, parse_mode="html")
