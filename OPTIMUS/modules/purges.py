449acccfb64290f7438a12de66ffe39742bd408d
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-04-22 11:51:12 +0530

    Update purges.py

--- OPTIMUS/modules/purges.py
+++ OPTIMUS/modules/purges.py
@@ -111,3 +111,80 @@ async def delete_replied(client, message):
     if message.reply_to_message:
         msg_ids.append(message.reply_to_message.message_id)
     await client.delete_messages(message.chat.id, msg_ids)
+
+@amaan.on_message(filters.command("purge", SUDO_HANDLER) & filters(SUDO_USERS))
+async def purge(client, message):
+    if message.reply_to_message:
+        datetime.now()
+        from_user = None
+        start_message = message.reply_to_message.message_id
+        end_message = message.message_id
+        list_of_messages = await client.get_messages(chat_id=message.chat.id,
+                                                    message_ids=range(start_message, end_message),
+                                                    replies=0)
+        list_of_messages_to_delete = []
+        purged_messages_count = 0
+        for a_message in list_of_messages:
+            if len(list_of_messages_to_delete) == 100:
+                await client.delete_messages(chat_id=message.chat.id,
+                                            message_ids=list_of_messages_to_delete,
+                                            revoke=True)
+                purged_messages_count += len(list_of_messages_to_delete)
+                list_of_messages_to_delete = []
+            if from_user is not None:
+                if a_message.from_user == from_user:
+                    list_of_messages_to_delete.append(a_message.message_id)
+            else:
+                list_of_messages_to_delete.append(a_message.message_id)
+        await client.delete_messages(chat_id=message.chat.id,
+                                    message_ids=list_of_messages_to_delete,
+                                    revoke=True)
+        purged_messages_count += len(list_of_messages_to_delete)
+        datetime.now()
+        await message.delete()
+    else:
+        await message.delete()
+
+
+@amaan.on_message(filters.command("purgeme", SUDO_HANDLER) & filters(SUDO_USERS))
+async def purge_myself(client, message):
+    if len(message.text.split()) >= 2 and message.text.split()[1].isdigit():
+        target = int(message.text.split()[1])
+    else:
+        await message.edit("Give me a number for a range!")
+    get_msg = await client.get_history(message.chat.id)
+    listall = []
+    counter = 0
+    for x in get_msg:
+        if counter == target + 1:
+            break
+        if x.from_user.id == int(Owner):
+            listall.append(x.message_id)
+            counter += 1
+    if len(listall) >= 101:
+        total = len(listall)
+        semua = listall
+        jarak = 0
+        jarak2 = 0
+        for x in range(math.ceil(len(listall) / 100)):
+            if total >= 101:
+                jarak2 += 100
+                await client.delete_messages(message.chat.id, message_ids=semua[jarak:jarak2])
+                jarak += 100
+                total -= 100
+            else:
+                jarak2 += total
+                await client.delete_messages(message.chat.id, message_ids=semua[jarak:jarak2])
+                jarak += total
+                total -= total
+    else:
+        await client.delete_messages(message.chat.id, message_ids=listall)
+
+
+@amaan.on_message(filters.command("del", SUDO_HANDLER) & filters(SUDO_USERS))
+async def delete_replied(client, message):
+    msg_ids = [message.message_id]
+    if message.reply_to_message:
+        msg_ids.append(message.reply_to_message.message_id)
+    await client.delete_messages(message.chat.id, msg_ids)
+
