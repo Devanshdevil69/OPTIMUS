74f897528e3aef9ec78e373deb63a5cbbae4ddc0
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-04-22 11:54:50 +0530

    Update ls.py

--- OPTIMUS/modules/ls.py
+++ OPTIMUS/modules/ls.py
@@ -9,6 +9,23 @@ __HELP__ = f"""
  
 """
 
+@amaan.on_message(filters.command("ls", SUDO_HANDLER) & filters(SUDO_USERS))
+async def ls(client, message):
+    args = message.text.split(None, 1)
+    if len(args) == 2:
+        basepath = "/app/{}".format(args[1])
+    else:
+        basepath = "/app/"
+    directory = ""
+    listfile = ""
+    for entry in os.listdir(basepath):
+        if os.path.isdir(os.path.join(basepath, entry)):
+            directory += "\n{}".format(entry)
+    for entry in os.listdir(basepath):
+        if os.path.isfile(os.path.join(basepath, entry)):
+            listfile += "\n{}".format(entry)
+    await message.edit_text("**List directory :**`{}`\n**List file :**`{}`".format(directory, listfile))
+
 @amaan.on_message(filters.command("ls", HANDLER) & filters.me)
 async def ls(client, message):
     args = message.text.split(None, 1)
