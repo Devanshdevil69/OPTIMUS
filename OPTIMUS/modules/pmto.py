791acdb2fceccc237072f7309d9ab71cf4662f45
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-04-26 13:06:02 +0530

    Update pmto.py

--- OPTIMUS/modules/pmto.py
+++ OPTIMUS/modules/pmto.py
@@ -6,7 +6,6 @@ import os
 from pyrogram import filters
 from pyrogram.types import Chat
 from OPTIMUS import amaan, HANDLER
-from OPTIMUS import SUDO_USERS, SUDO_HANDLER
 
 __PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
 __HELP__ = f"""
@@ -16,27 +15,6 @@ __HELP__ = f"""
 
 
 @amaan.on_message(filters.command("pmto", HANDLER) & filters.me)
-async def pmto(client, message):
-    a = message.pattern_match.group(1)
-    b = a.split(" ")
-    chat_id = b[0]
-    try:
-        chat_id = int(chat_id)
-    except BaseException:
-        pass
-    msg = ""
-    for i in b[1:]:
-        msg += i + " "
-    if msg == "":
-        return
-    try:
-        await client.send_message(chat_id, msg)
-        await message.edit_text("Message sent!")
-    except BaseException:
-        await message.edit_text("Something went wrong.")
-
-
-@amaan.on_message(filters.command("pmto", SUDO_HANDLER) & filters(SUDO_USERS))
 async def pmto(client, message):
     a = message.pattern_match.group(1)
     b = a.split(" ")
