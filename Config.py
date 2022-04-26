0d04aa807e908dbc616a098b519f68e94324a0da
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-04-26 13:23:50 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,8 +1,8 @@
 import os
+
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 SESSION_STRING = os.environ.get("SESSION_STRING")
 HANDLER = os.environ.get("HANDLER")
 OWNER_ID = int(os.environ.get("OWNER_ID"))
-SUDO_HANDLER = os.environ.get("SUDO_HANDLER")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "511112479").split()))
+SUDO_USERS = ?
