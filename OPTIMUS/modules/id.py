278c6ef86d2124c216a39fa3eb6da0417980e589
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-04-26 13:15:02 +0530

    Update id.py

--- OPTIMUS/modules/id.py
+++ OPTIMUS/modules/id.py
@@ -1,7 +1,6 @@
 import os
 from OPTIMUS import amaan, HANDLER
 from pyrogram import filters
-from OPTIMUS import SUDO_USERS, SUDO_HANDLER
 
 __PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
 __HELP__ = f"""
@@ -11,64 +10,6 @@ __HELP__ = f"""
 """
 
 @amaan.on_message(filters.command("id", HANDLER) & filters.me)
-async def get_id(client, message):
-    file_id = None
-    user_id = None
-
-    if message.reply_to_message:
-        rep = message.reply_to_message
-        if rep.audio:
-            file_id = rep.audio.file_id
-        elif rep.document:
-            file_id = rep.document.file_id
-        elif rep.photo:
-            file_id = rep.photo.file_id
-        elif rep.sticker:
-            file_id = rep.sticker.file_id
-        elif rep.video:
-            file_id = rep.video.file_id
-        elif rep.animation:
-            file_id = rep.animation.file_id
-        elif rep.voice:
-            file_id = rep.voice.file_id
-        elif rep.video_note:
-            file_id = rep.video_note.file_id
-        elif rep.contact:
-            file_id = rep.contact.file_id
-        elif rep.location:
-            file_id = rep.location.file_id
-        elif rep.venue:
-            file_id = rep.venue.file_id
-        elif rep.from_user:
-            if rep.forward_from:
-                user_id = rep.forward_from.id
-                if rep.forward_from.last_name:
-                    user_name = (
-                        rep.forward_from.first_name + " " + rep.forward_from.last_name
-                    )
-                else:
-                    user_name = rep.forward_from.first_name
-                username = rep.forward_from.username
-            else:
-                user_id = rep.from_user.id
-                if rep.from_user.last_name:
-                    user_name = rep.from_user.first_name + " " + rep.from_user.last_name
-                else:
-                    user_name = rep.from_user.first_name
-                username = rep.from_user.username
-
-    if user_id:
-        await message.edit_text(
-            "**User ID:** `{}`\n**Name:** `{}`\n**Username:** @{}".format(
-                user_id, user_name, username
-            )
-        )
-    elif file_id:
-        await message.edit_text(f"**File's ID:** `{file_id}`")
-    else:
-        await message.edit_text(f"**This Chat's ID:** `{m.chat.id}`")
-
-@amaan.on_message(filters.command("id", SUDO_HANDLER) & filters(SUDO_USERS))
 async def get_id(client, message):
     file_id = None
     user_id = None
