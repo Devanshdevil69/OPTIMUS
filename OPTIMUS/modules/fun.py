2908432be8ec24df5a56c19ef1f39bf928b220c5
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-05-12 20:13:27 +0530

    Update fun.py

--- OPTIMUS/modules/fun.py
+++ OPTIMUS/modules/fun.py
@@ -12,7 +12,7 @@ __HELP__ = f"""
 
 # KiLL
 
-@amaan.on_message(filters.command("kill", HANDLER) & filters.me & filters.user(SUDO_USERS))
+@amaan.on_message(filters.command("kill", HANDLER) & filters.private & filters.user(SUDO_USERS))
 async def kill(client, message):
    await message.reply_text("UseR KilleD Sucessfully  ▀̿ ̿Ĺ̯̿̿▀̿ ̿")
 
