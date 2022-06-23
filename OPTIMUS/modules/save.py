c32858fb70b9cd1bf43ed9d8dfaf82b292112cb1
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-23 13:49:58 +0530

    Update save.py

--- OPTIMUS/modules/save.py
+++ OPTIMUS/modules/save.py
@@ -1,6 +1,6 @@
 import os
 from pyrogram import filters
-from OPTIMUS import amaan, HANDLER
+from OPTIMUS import amaan, HANDLER, SUDO_USERS
 
 
 __PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
@@ -10,6 +10,7 @@ USAGE :- {HANDLER}save
 """ 
 
 @amaan.on_message(filters.command("save", HANDLER) & filters.me)
+@amaan.on_message(filters.command("save", HANDLER) & filters.user(SUDO_USERS))
 async def to_saved(client, message):
     await message.reply_to_message.forward("self")
     await message.edit_text('`Message Has BeeN SaveD.`')
