b8beda2e24b32ef349e1b11c69a7a24f87ef9d37
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-23 13:39:33 +0530

    Update forwarder.py

--- OPTIMUS/modules/forwarder.py
+++ OPTIMUS/modules/forwarder.py
@@ -4,10 +4,11 @@
 
 import os
 import asyncio
-from OPTIMUS import amaan, HANDLER
+from OPTIMUS import amaan, HANDLER ,SUDO_USERS
 from pyrogram import filters
 
 @amaan.on_message(filters.command("forward", HANDLER) & filters.me)
+@amaan.on_message(filters.command("forward", HANDLER) & filters.user(SUDO_USERS))
 async def forwader(client, message):
    a = await amaan.get_history(777000, limit=1)
    print(a[0].text)
