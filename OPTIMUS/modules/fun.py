7ec4afc0584443f4e333d50f6355cb2c427e1a19
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-23 13:40:49 +0530

    Update fun.py

--- OPTIMUS/modules/fun.py
+++ OPTIMUS/modules/fun.py
@@ -12,13 +12,15 @@ __HELP__ = f"""
 
 # KiLL
 
-@amaan.on_message(filters.command("kill", HANDLER) & filters.private & filters.user(SUDO_USERS))
+@amaan.on_message(filters.command("kill", HANDLER) & filters.me)
+@amaan.on_message(filters.command("kill", HANDLER) & filters.user(SUDO_USERS))
 async def kill(client, message):
    await message.reply_text("UseR KilleD Sucessfully  ▀̿ ̿Ĺ̯̿̿▀̿ ̿")
 
 # MF
 
 @amaan.on_message(filters.command("mf", HANDLER) & filters.me)
+@amaan.on_message(filters.command("mf", HANDLER) & filters.user(SUDO_USERS))
 async def mf(client, message):
    await message.edit_text(
             "\n...................................../´¯/) "
