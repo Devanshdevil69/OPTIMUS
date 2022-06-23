3473a12ac6e305b987342c89c5458ca1f0c39f96
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-23 13:51:25 +0530

    Update space.py

--- OPTIMUS/modules/space.py
+++ OPTIMUS/modules/space.py
@@ -1,6 +1,6 @@
 import os
 from pyrogram import filters
-from OPTIMUS import amaan, HANDLER
+from OPTIMUS import amaan, HANDLER, SUDO_USERS
 
 __PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
 __HELP__ = f"""
@@ -15,12 +15,14 @@ USAGE :-
 # S P A C E
 
 @amaan.on_message(filters.command("space", HANDLER) & filters.me)
+@amaan.on_message(filters.command("space", HANDLER) & filters.user(SUDO_USERS))
 async def space(client, message):
     await message.edit_text("ㅤ")
 
 # B L A N K
 
 @amaan.on_message(filters.command("blank", HANDLER) & filters.me)
+@amaan.on_message(filters.command("blank", HANDLER) & filters.user(SUDO_USERS))
 async def blank(client, message):
     await message.edit_text("­")
 
