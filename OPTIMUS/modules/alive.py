d98b356c855a9d8623f9e08655cb423e4e8a6bce
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2023-04-02 21:29:15 +0530

    Update alive.py

--- OPTIMUS/modules/alive.py
+++ OPTIMUS/modules/alive.py
@@ -36,8 +36,4 @@ async def alive(client, message):
     text += f"\nPyrogram Version: `{__version__}`"
     text += f"\nCurrent Uptime: `{str(datetime.now() - StartTime).split('.')[0]}`"
 
-    await client.send_photo(message.chat.id, ALIVE_PIC, caption=text)
-if Exception:
-    pass
-try:
-    await client.send_video(message.chat.id, ALIVE_PIC, caption=text)
+    await client.send_media_group(message.chat.id, ALIVE_PIC, caption=text)
