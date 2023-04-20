8511d34fb58c8d52c2a0af12c0a4be5bc4dc7ecc
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2023-04-20 18:18:51 +0530

    Update eval.py

--- OPTIMUS/modules/eval.py
+++ OPTIMUS/modules/eval.py
@@ -33,9 +33,9 @@ async def evaluate(client, message):
     except IndexError:
         await status_message.delete()
         return
-    reply_to_id = Message.message_id
-    if Message.reply_to_message:
-        reply_to_id = Message.reply_to_message.message_id
+    reply_to_id = message.id
+    if message.reply_to_message:
+        reply_to_id = message.reply_to_message.message.id
     old_stderr = sys.stderr
     old_stdout = sys.stdout
     redirected_output = sys.stdout = StringIO()
