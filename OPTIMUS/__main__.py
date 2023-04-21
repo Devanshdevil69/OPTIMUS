9e32a2e1f7fa1a516da936990daf9e55c019a9ff
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2023-04-21 11:46:18 +0530

    Update __main__.py

--- OPTIMUS/__main__.py
+++ OPTIMUS/__main__.py
@@ -1,12 +1,14 @@
 import os
-from OPTIMUS import *
+from OPTIMUS import LOGGER, load_cmds
 from OPTIMUS import amaan
 
-from OPTIMUS.modules import *
+from OPTIMUS.modules import ALL_PLUGINS
+from pyrogram import Client
 
-
-
-
+async def start(self):
+    await super().start()
+    result = load_cmds(ALL_PLUGINS)
+    log.info(result)
 
 if __name__ == "__main__":
     amaan.run()
